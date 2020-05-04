1445b2b93d62e162cd5a0d17db5a23d72608243b
Ajoy Ranga <RANGAJY@LC02VL1BEHTDD>
2020-05-03 21:48:29 -0400

    implemented reselect selectors for cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
 import { CartItem } from '../cart-item/cart-item.component';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
-          {
-              cartItems.map(cartItem => (
-                  <CartItem key={cartItem.id} item={cartItem} />
-              ))
-          }
+            {
+                cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem} />
+                ))
+            }
         </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-const mapStateToProps = ({cart: {cartItems}}) => (
+const mapStateToProps = (state) => (
     {
-        cartItems
+        cartItems: selectCartItems(state)
     }
 )
 export default connect(mapStateToProps)(CartDropdown);
