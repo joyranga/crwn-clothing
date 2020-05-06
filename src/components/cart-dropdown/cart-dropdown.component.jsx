d5279ef4d729aebdc6481a5474e846d4760506e3
Ajoy Ranga <RANGAJY@LC02VL1BEHTDD>
2020-05-05 23:10:09 -0400

    creaetd the checkout-item component and finished the checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,11 +7,12 @@ import CustomButton from '../custom-button/custom-button.component';
 import { CartItem } from '../cart-item/cart-item.component';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         {
             cartItems.length ?
@@ -25,7 +26,12 @@ const CartDropdown = ({ cartItems, history }) => (
                 :
                 <span className='empty-message'>Your Cart is empty</span>
         }
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={
+            () => {
+                history.push('/checkout');
+                dispatch(toggleCartHidden());
+            }
+        }>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -3,10 +3,26 @@ export const addItemToCart = (cartItems, itemToAdd) => {
     const itemExists = cartItems.find(item => item.id === itemToAdd.id);
     if (itemExists) {
         return cartItems.map(item => (
-            item.id === itemToAdd.id ? {...item, quantity: item.quantity + 1} : item
+            item.id === itemToAdd.id ? { ...item, quantity: item.quantity + 1 } : item
         ))
     }
     else {
         return [...cartItems, { ...itemToAdd, quantity: 1 }];
     }
-}
+};
+
+export const clearItemFromCart = (cartItems, itemToClear) => (
+    cartItems.filter(item => item.id !== itemToClear.id)
+);
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const cartItem = cartItems.find(item => item.id === cartItemToRemove.id);
+    if(cartItem.quantity === 1) {
+       return cartItems.filter(item => item.id !== cartItemToRemove.id)
+    }
+    return cartItems.map(item => (
+        item.id === cartItemToRemove.id ? { ...item, quantity: item.quantity - 1 } : item
+    ))
+
+};
+
